9e870ea9a0920c61b06eb96d24dac19243b4286a
CJ <kubachod@gmail.com>
2023-03-07 20:32:58 +0100

    Initial Commit

---
+++ Platoon Team Manager/Platoon_Team_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Platoon_Team_ManagerApp.swift
+//  Platoon Team Manager
+//
+//  Created by Jakub Chodara on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Platoon_Team_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
